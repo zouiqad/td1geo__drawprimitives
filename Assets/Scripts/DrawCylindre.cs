6759926497c86bb68c3ffeefa14b8ca1f3ce877a
zouiqad <lebsaira.wassil@gmail.com>
2023-10-09 17:27:04 +0200

    Initial commit

---
+++ Assets/Scripts/DrawCylindre.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DrawCylin : MonoBehaviour
+{
+    public Material mat;
+    public Quaternion currentRotation;
+
+    public float rayon = 2.0f;
+    public float h = 5.0f;
+    public int meridian = 3;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DrawCylindre(meridian, rayon, h);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void DrawCylindre(int m, float r, float h)
+    {
+        Vector3[] vertices = new Vector3[2 + m*2] ;
+        int[] triangles = new int[m*2*3*2];
+
+
+        if (gameObject.GetComponent<MeshFilter>() == null)
+        {
+            gameObject.AddComponent<MeshFilter>();          // Creation d'un composant MeshFilter qui peut ensuite être visualisé
+            gameObject.AddComponent<MeshRenderer>();
+        }
+
+        // Generate vertices
+        int vertexIndex = 0;
+        float phi = (2 * Mathf.PI) / m;
+        float teta = 0.0f;
+
+        for (int i = 0; i < m; i++)
+        {
+            
+            Vector3 p = new Vector3(r * Mathf.Cos(teta), h/2, r * Mathf.Sin(teta));
+            Vector3 pp = new Vector3(r * Mathf.Cos(teta), -1*h/2, r * Mathf.Sin(teta));
+
+            vertices[vertexIndex++] = p;
+            vertices[vertexIndex++] = pp;
+
+            teta += phi;
+            Debug.Log(teta);
+            Debug.Log(p);
+            Debug.Log(pp);
+
+        }
+
+        
+        // Generate triangles
+
+        int triangleIndex = 0;
+
+        // Draw Body
+
+        for (int i = 0; i < (m-1); i++)
+        {
+            int vertexA = i * 2;
+            int vertexB = vertexA + 1;
+            int vertexC = vertexB + 1;
+            int vertexD = vertexC + 1;
+
+
+            triangles[triangleIndex++] = vertexA;
+            triangles[triangleIndex++] = vertexD;
+            triangles[triangleIndex++] = vertexB;
+
+            triangles[triangleIndex++] = vertexA;
+            triangles[triangleIndex++] = vertexC;
+            triangles[triangleIndex++] = vertexD;
+        }
+
+        triangles[triangleIndex++] = (m * 2) - 2;
+        triangles[triangleIndex++] = 1;
+        triangles[triangleIndex++] = (m * 2) - 1;
+
+        triangles[triangleIndex++] = (m * 2) - 2;
+        triangles[triangleIndex++] = 0;
+        triangles[triangleIndex++] = 1;
+
+
+
+        Vector3 origin_top = new Vector3(0.0f, h/2, 0.0f);
+        Vector3 origin_bot = new Vector3(0.0f, -1 * h / 2, 0.0f);
+
+
+        vertices[vertexIndex++] = origin_top;
+        vertices[vertexIndex++] = origin_bot;
+
+        // Draw Top
+        for (int i = 0; i <= m; i+=2)
+        {
+            triangles[triangleIndex++] = vertices.Length - 2;
+            triangles[triangleIndex++] = i + 2;
+            triangles[triangleIndex++] = i + 2;
+        }
+
+        // Draw Bot
+        for (int i = 1; i <= m; i += 2)
+        {
+            triangles[triangleIndex++] = vertices.Length - 1;
+            triangles[triangleIndex++] = i + 2;
+            triangles[triangleIndex++] = i;
+        }
+
+        Mesh msh = new Mesh();                          // Création et remplissage du Mesh
+
+        msh.vertices = vertices;
+        msh.triangles = triangles;
+
+        gameObject.GetComponent<MeshFilter>().mesh = msh;           // Remplissage du Mesh et ajout du matériel
+        gameObject.GetComponent<MeshRenderer>().material = mat;
+
+    }
+}
---
+++ Assets/Scripts/DrawTriangle.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+
+public class Hello_Triangle : MonoBehaviour
+{
+
+    public Material mat;
+    public Quaternion currentRotation;
+
+    public int nb_ligne = 2;
+    public int nb_colonne = 2;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        drawTriangle(nb_ligne, nb_colonne);
+    }
+
+    void Update()
+    {
+
+        drawTriangle(nb_ligne, nb_colonne);
+    }
+    
+
+    private void drawTriangle(int n, int m)
+    {
+
+        if (gameObject.GetComponent<MeshFilter>() == null)
+        {
+            gameObject.AddComponent<MeshFilter>();          // Creation d'un composant MeshFilter qui peut ensuite être visualisé
+            gameObject.AddComponent<MeshRenderer>();
+        }
+        int nb_triangles = n * m * 2;
+        Vector3[] vertices = new Vector3[nb_triangles * 3];
+        int[] triangles = new int[nb_triangles * 3];
+
+
+        // Generate vertices
+        int vertexIndex = 0;
+
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= m; j++)
+            {
+                vertices[vertexIndex++] = new Vector3((float)i, (float)j, 0.0f);
+            }
+        }
+
+        // Generate triangles 
+        int triangleIndex = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                int vertexA = i * (m + 1) + j;
+                int vertexB = vertexA + 1;
+                int vertexC = (m + 1) * (i + 1) + j;
+                int vertexD = vertexC + 1;
+
+                triangles[triangleIndex++] = vertexA;
+                triangles[triangleIndex++] = vertexB;
+                triangles[triangleIndex++] = vertexD;
+
+                triangles[triangleIndex++] = vertexA;
+                triangles[triangleIndex++] = vertexD;
+                triangles[triangleIndex++] = vertexC;
+
+            }
+        }
+
+
+
+        Mesh msh = new Mesh();                          // Création et remplissage du Mesh
+
+        msh.vertices = vertices;
+        msh.triangles = triangles;
+
+        gameObject.GetComponent<MeshFilter>().mesh = msh;           // Remplissage du Mesh et ajout du matériel
+        gameObject.GetComponent<MeshRenderer>().material = mat;
+    }
+
+}
+
